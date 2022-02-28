eb3453c35f389a600987b486db6c03b15eb75d4a
Kike <norberto.00cardenas@gmail.com>
2022-02-28 11:45:36 -0500

    Avance 4
    Semava

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -10,11 +10,46 @@
 
     .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
       .bloque-texto-g__img(
-        :style="{'background-image': `url(${require('@/assets/curso/t1/1.png')})`}"
+        :style="{'background-image': `url(${require('@/assets/curso/tema1/img01.png')})`}"
       )
-      .bloque-texto-g__texto.p-4
-        p.mb-0 De acuerdo con Gonzáles y Pelissier (2002), los métodos o funciones permiten segmentar el trabajo que hace un programa en subtareas o tareas más pequeñas, enfocadas en un fin específico, y se pueden utilizar cuantas veces se necesite; estas están separadas del programa principal, pero aportan a su objetivo.
-      
+      .bloque-texto-g__texto.p-5
+        p.mb-0 
+        span.text-black De acuerdo con Gonzáles y Pelissier (2002),
+        |los métodos o funciones permiten segmentar el trabajo que hace un programa en subtareas o tareas más pequeñas,enfocadas en un fin específico, y se pueden utilizar cuantas veces se necesite;estas están separadas del programa principal, pero aportan a su objetivo.
+    <br>
+    <br>     
+    .col-lg-4
+      .cajon.color-acento-botones.p-4.mb-4
+        p El uso de funciones da la capacidad de agrupar varias instrucciones de código bajo un solo nombre, y con el nombre de la función, es posible invocarlo las veces que se necesite, permitiendo usar esta función las veces que sean necesarias.
+    .div.row.mt-4.mb-4
+      .div.col-md-6
+        img(src='@/assets/curso/tema1/1.svg', alt='Texto que describa la imagen')
+      .div.col-md-6
+        .h2.titulo-tercero Ejemplo 1:
+        p La funcion se define con la siguiente sintaxis.
+        .div.cajon.color-secundario.p-4.mb-4
+          div.h5 Nota:
+          | el símbolo “//” quiere decir que no hace parte del código, se lo utiliza para hacer comentarios sobre el código, por lo cual se pone en color azul.
+        .ol.lista-ol
+          img(src='@/assets/curso/tema1/zz.png', alt='Texto que describa la imagen')  
+
+
+    .div.row.mt-4.mb-4
+
+      .div.col-md-6
+        p La función arroja como resultados los números 8 y 400.
+        p En el ejemplo anterior, se observa la sintaxis de una función que arroja la media aritmética de dos o más números, pero es necesario recordar qué es la media aritmética. Según Paz (s. f.), “La media aritmética o promedio simple se calcula sumando los valores de interés y dividiendo entre el número de valores sumados”. Para el anterior caso, se suman 2 números y el resultado se divide entre 2, así:
+        .div.col-md-4.mb-10.mb-md-0
+          img(src='@/assets/curso/tema1/mate.png', alt='Texto que describa la imagen')  
+      .div.col-md-6
+        img(src='@/assets/curso/tema1/2.svg', alt='Texto que describa la imagen')
+
+    <br>
+    .col-lg-4
+      .cajon.color-acento-botones.p-4.mb-4
+        p-span Los autores, además, dicen que estos tienen 3 tipos de visibilidad: 
+        p-span.text-black Public “Pública”, Private “Privada”, o Protected “Protegidas” 
+        | , si no se especifica, se sobreentiende que es privada.
 
 </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,6 +7,67 @@
       .titulo-principal__numero
         span 2
       h1 Clases y objetos  
+    .div.row.mt-5.mb-1
+      .div.col-4
+        img(src='@/assets/curso/tema2/1.svg', alt='Texto que describa la imagen')
+        <br>
+      .div.col-8.mt-4
+        .div.row
+          .div.col-11
+            .div.cajon.mb-4
+              p Las clases son contenedores de información basados en atributos y en métodos de construcción, los cuales pueden modificar y consultar; son suficientes para representar objetos con los cuales se procesa información, y se usan para hacer objetos que tienen un mismo comportamiento, estado e identidad.
+        <br>
+        .div.col-md-11
+          .h2.titulo-tercero Ejemplo 2:
+          p Se tiene una clase “personas” (Mario, Margarita y Alejandro), donde el comportamiento de las personas es leer, trabajar, jugar, etc.; pueden estar en estado despierto o dormido; sus propiedades pueden ser color de ojos, género, estado civil, etc.
+          p Su sintaxis sería así:
+          <br>
+          p.mb-0 class Persona { 
+          p.mb-0 # Propiedades 
+          p.mb-0 # Métodos 
+          p }
+        div.row.mb-2
+          div.col-md-2.p-0
+          div.col-md-9 
+            <br>
+            p.mb-3 <strong>Objeto:</strong> el objeto es una instancia de una clase, la cual puede crear varias instancias de la misma clase.
+            p.mb-3 <strong>Ejemplo:</strong> existe una sola clase “Persona”, pero muchos objetos de tipo persona pueden ser instancias de esta clase.
+    <br>
+    <br>
+        h2.text-center.mb-0 A continuación, observe la diferencia entre 
+        h2.text-center clases y objetos con una analogía:
+        <br>
+
+
+
+    div.row.mb-4.mt-4.justify-content-center
+      div.col-md-7
+        img(src='@/assets/curso/tema2/3.svg', alt='Texto que describa la imagen')
+        <br>
+        img(src='@/assets/curso/tema2/5.png', alt='Texto que describa la imagen')
+
+    AcordionA(tipo="b" clase-tarjeta="tarjeta tarjeta")
+      .row(titulo="Analogía")
+        .col-md-12
+          figure
+            img(src='@/assets/curso/tema2/6.png', alt='Texto que describa la imagen')
+
+      .row(titulo="Ejemplo")
+        .col-md-7.mb-4.mb-md-0
+          p En el siguiente ejemplo, se observará el código de un objeto. 
+          p El símbolo “//” quiere decir que no hace parte del código, se lo utiliza para hacer comentarios sobre el código, por lo cual, se pone en color azul.
+          .col-md-7.mb-4.mb-md-0
+            img(src='@/assets/curso/tema2/7.png', alt='Texto que describa la imagen')
+        .col-md-5
+          figure
+            img(src='@/assets/curso/tema2/4.svg', alt='Texto que describa la imagen')
+
+      div(titulo="Resultado")
+        p Si se corriera el código, daría este resultado.
+        <br>
+            img(src='@/assets/curso/tema2/8.png', alt='Texto que describa la imagen')
+
+
 
 </template>
 
